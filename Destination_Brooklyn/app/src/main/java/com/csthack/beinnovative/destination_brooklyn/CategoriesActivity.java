e90bbec0487799affd00c9f3d52713780e087488
Berta Devant <bertadeva@gmail.com>
2016-04-09 13:05:29 -0400

    More on Category activity

--- Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/CategoriesActivity.java
+++ Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/CategoriesActivity.java
@@ -1,22 +1,45 @@
 package com.csthack.beinnovative.destination_brooklyn;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CategoriesActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+
+public class CategoriesActivity extends FragmentActivity
+        implements OnMapReadyCallback {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        //setSupportActionBar(toolbar);
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
