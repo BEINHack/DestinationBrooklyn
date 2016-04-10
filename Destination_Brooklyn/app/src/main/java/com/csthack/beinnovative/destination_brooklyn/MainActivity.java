158920d6232dc5a694d387a60285e35a74dc5ded
topher91 <cpetta@Chriss-MacBook-Air.local>
2016-04-09 20:37:50 -0400

    Added working map markers
    did you know that when doing a for loop to loop through arrays you need
    to do < array.length… not <= array.length? because apparently I did not

--- Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/MainActivity.java
+++ Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/MainActivity.java
@@ -4,6 +4,9 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 
 import android.content.Intent;
@@ -16,12 +19,24 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
 import com.csthack.beinnovative.destination_brooklyn.pointInterestClass;
+
 public class MainActivity extends AppCompatActivity
         implements
         OnMyLocationButtonClickListener,
         OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback {
+
+//    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+//
+//    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+//
+//    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+//
+//    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+//
+//    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
 
     /**
      * Request code for location permission request.
@@ -35,7 +50,7 @@ public class MainActivity extends AppCompatActivity
      * {@link #onRequestPermissionsResult(int, String[], int[])}.
      */
     private boolean mPermissionDenied = false;
-
+    private double lat, lon;
     private GoogleMap mMap;
 
     @Override
@@ -46,19 +61,63 @@ public class MainActivity extends AppCompatActivity
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        POIdata start = new POIdata();
-        pointInterestClass [] myObjects = start.getPOIobjects();
-        double lat = myObjects[1].getLatitude();
-        double lon = myObjects[1].getLatitude();
+//        POIdata start = new POIdata();
+//        pointInterestClass[] myObjects = start.getPOIobjects();
+//        int length = myObjects.length;
+        //System.out.println(length);
+
+
+//        String periodSelected = getIntent().getExtras().getString("buildingType");
+//        String timeSelected = getIntent().getExtras().getString("TimePeriod");
+//        for (int i = 0; i <= length; i++) {
+//            lat = myObjects[i].getLatitude();
+//            lon = myObjects[i].getLongitude();
+//            mMap.addMarker(new MarkerOptions()
+//                    .position(new LatLng(lat, lon))
+//                    .title(myObjects[i].getName()));
+//        }
+//
+//
+//        if (periodSelected.equals(null) && timeSelected.equals(null)) {
+//            for (int i = 0; i <= myObjects.length; i++) {
+//                lat = myObjects[i].getLatitude();
+//                lon = myObjects[i].getLatitude();
+//                mMap.addMarker(new MarkerOptions()
+//                        .position(new LatLng(lat, lon))
+//                        .title(myObjects[i].getName()));
+//            }
+//        }
+
     }
 
 
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
-
         mMap.setOnMyLocationButtonClickListener(this);
-        //enableMyLocation();
+
+
+        POIdata start = new POIdata();
+        pointInterestClass[] myObjects = start.getPOIobjects();
+        int length = myObjects.length;
+
+//        lat = myObjects[1].getLatitude();
+//            lon = myObjects[1].getLongitude();
+//            map.addMarker(new MarkerOptions()
+//                    .position(new LatLng(lat, lon))
+//                    .title(myObjects[1].getName()));
+
+
+
+        for (int i = 0; i < length; i++) {
+            lat = myObjects[i].getLatitude();
+            lon = myObjects[i].getLongitude();
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(lat, lon))
+                    .title(myObjects[i].getName()));
+        }
+
+        enableMyLocation();
     }
 
     /**
@@ -124,7 +183,7 @@ public class MainActivity extends AppCompatActivity
      * Adding menu bar
      */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_activity_menu, menu);
         return super.onCreateOptionsMenu(menu);
@@ -133,7 +192,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent launchActivity = new Intent();
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.filter_id:
                 launchActivity = new Intent(this, CategoriesActivity.class);
                 break;
@@ -148,11 +207,12 @@ public class MainActivity extends AppCompatActivity
                 break;
 
             case R.id.search_id:
-                Toast.makeText(getApplicationContext(),"Allow user to search a specific address/ subject", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Allow user to search a specific address/ subject", Toast.LENGTH_SHORT).show();
                 break;
         }
         startActivity(launchActivity);
         return super.onOptionsItemSelected(item);
     }
+
 
 }
