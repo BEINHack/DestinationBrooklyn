eb281cee69a3d4219b141d835bd174fad0fb1c61
Berta Devant <bertadeva@gmail.com>
2016-04-10 04:29:27 -0400

    Some styling things for the shop list

--- Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/CustomListAdapter.java
+++ Destination_Brooklyn/app/src/main/java/com/csthack/beinnovative/destination_brooklyn/CustomListAdapter.java
@@ -2,73 +2,125 @@ package com.csthack.beinnovative.destination_brooklyn;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Movie;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.csthack.beinnovative.destination_brooklyn.shopClass;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Berta on 4/9/2016.
  */
-public class CustomListAdapter extends BaseAdapter {
-    private Activity activity;
-    private LayoutInflater inflater;
-    private shopClass[] shopObjects;
-    //ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+public class CustomListAdapter extends ArrayAdapter<shopClass> {
+    ArrayList<shopClass> shopObjects;
+    private final int resource;
 
-    public CustomListAdapter(Activity activity, shopClass[] shopObjects) {
-        this.activity = activity;
-        this.shopObjects = shopObjects;
-    }
-    @Override
-    public int getCount() {
-        return shopObjects.length;
-    }
-    @Override
-    public shopClass getItem(int location) {
-        return shopObjects[location];
-    }
+    public CustomListAdapter(Context context, ArrayList<shopClass> PshopObjects, int resource) {
+        super(context, R.layout.list_row, PshopObjects);
+        shopObjects = PshopObjects;
+        this.resource = resource;
 
-    @Override
-    public long getItemId(int position) {
-        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        if (inflater == null)
-            inflater = (LayoutInflater) activity
-                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if (convertView == null)
-            convertView = inflater.inflate(R.layout.list_row, null);
+        // We need to get the best view (re-used if possible) and then
+        // retrieve its corresponding ViewHolder, which optimizes lookup efficiency
+        View view = getWorkingView(convertView);
+        ViewHolder viewHolder = getViewHolder(view);
+        shopClass shop = getItem(position);
+        System.out.println("Item Position: "+getItem(position));
 
-//        if (imageLoader == null)
-//            imageLoader = AppController.getInstance().getImageLoader();
-//        NetworkImageView thumbNail = (NetworkImageView) convertView
-//                .findViewById(R.id.thumbnail);
-        TextView title = (TextView) convertView.findViewById(R.id.title);
-        TextView description = (TextView) convertView.findViewById(R.id.description);
-        LinearLayout bkgImage = (LinearLayout) convertView.findViewById(R.id.bkgImage);
+//        if (view == null) {
+//            view = getLayoutInflater().inflate(R.layout.list_row, parent, false);
+//        }
 
-        // getting movie data for the row
-        shopClass shop = shopObjects[position];
-        // bkg image
-//        bkgImage.setBackground();
+        if (shopObjects == null) {
+            System.out.println("it is null on CustomListarrya");
+        }
 
-        // title
-        title.setText(shop.getName());
+        // Setting the title view is straightforward
+        viewHolder.titleView.setText(shop.getName());
+        viewHolder.subTitleView.setText(shop.getDescription());
+        //viewHolder.imageView.setBackground();
 
-        // description
-        description.setText(shop.getDescription());
-        return convertView;
+//        TextView title = (TextView) alertView.findViewById(R.id.title);
+//        TextView description = (TextView) alertView.findViewById(R.id.description);
+//        LinearLayout bkgImage = (LinearLayout) alertView.findViewById(R.id.bkgImage);
+//
+//        // getting movie data for the row
+//
+//        // bkg image
+////        bkgImage.setBackground();
+//
+//        // title
+//        title.setText(shop.getName());
+//
+//        // description
+//        description.setText(shop.getDescription());
+        return view;
+    }
+
+    private View getWorkingView(final View convertView) {
+        // The workingView is basically just the convertView re-used if possible
+        // or inflated new if not possible
+        View workingView = null;
+
+        if(null == convertView) {
+            final Context context = getContext();
+            final LayoutInflater inflater = (LayoutInflater)context.getSystemService
+                    (Context.LAYOUT_INFLATER_SERVICE);
+
+            workingView = inflater.inflate(resource, null);
+        } else {
+            workingView = convertView;
+        }
+
+        return workingView;
+    }
+
+    private ViewHolder getViewHolder(final View workingView) {
+        // The viewHolder allows us to avoid re-looking up view references
+        // Since views are recycled, these references will never change
+        final Object tag = workingView.getTag();
+        ViewHolder viewHolder = null;
+
+
+        if(null == tag || !(tag instanceof ViewHolder)) {
+            viewHolder = new ViewHolder();
+
+            viewHolder.titleView = (TextView) workingView.findViewById(R.id.title);
+            viewHolder.subTitleView = (TextView) workingView.findViewById(R.id.description);
+            viewHolder.imageView = (LinearLayout) workingView.findViewById(R.id.bkgImage);
+
+            workingView.setTag(viewHolder);
+
+        } else {
+            viewHolder = (ViewHolder) tag;
+        }
+
+        return viewHolder;
+    }
+
+    /**
+     * ViewHolder allows us to avoid re-looking up view references
+     * Since views are recycled, these references will never change
+     */
+    private static class ViewHolder {
+        public TextView titleView;
+        public TextView subTitleView;
+        public LinearLayout imageView;
     }
 
 }
